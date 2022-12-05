6be07c47b245175277cd24a4e0680bf092209bd7
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2022-12-05 19:45:17 +0800

    bugs fixes + new features
    added README
    edited commands

---
+++ README.md
@@ -0,0 +1,31 @@
+# ryans_bot\_ commands
+
+## Information about streamer:
+
+-    !intro or !about
+-    !website
+
+## Interaction with users:
+
+-    !hug \@user
+-    !pat \@user
+-    !bonk \@user
+-    !kabedon \@user
+
+## Socials:
+
+-    !socials
+-    !github
+-    !discord
+
+## Task bot: (source code from different repository)
+
+-    !taskhelp
+-    !task \[task\]
+-    !remove
+
+## Broadcaster commands:
+
+-    raid
+-    raid2
+-    clear (to clear task bot)
--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const ComfyJS = require("comfy.js");
 require("dotenv").config();
 
+
 const commands = {
-    'overlays':`{user} I made the overlays and bot myself! I used HTML + CSS and Javascript (ComfyJS) to make it! Please leave some feedback on it, would love some advice!`,
     'taskhelp':`{user} "!task [task]" to add your task to the list; "!done" when you're done with your task; "!remove" if you made a typo in the previous task.`,
-    'wordle':`I made Wordle on Stream, a game where users can do "!playwordle" then enter any 5 letter words to play the game and "!stopwordle" to stop playing the game.`,
     'lurk':`Thanks for lurking, {user}!`,
     'unlurk':`Welcome back {user}!`,
     'socials':`{user} here are Ryan's socials: https://liyunze-coding.github.io/socials.html`,
     'github':`{user} https://github.com/liyunze-coding`,
-    'welcome':`{user} Welcome to Ryan's comfy stream, hope you enjoy your time here :D`,
+    'welcome':`{message} Welcome to Ryan's comfy stream, hope you enjoy your time here :D`,
     'website':`{user} Check out Ryan's website here: https://liyunze-coding.github.io/`,
     'hug':'{user} sends a hug to {message} <3 <3',
     'pat':"{user} pats {message} <3 <3",
     'bonk':'{user} bonks {message} >:(',
-    'about':`{user} I'm Ryan currently living in Asia, I do co-working streams and I am studying computer science in college :D`,
-    'discord':`{user} Come join the Discord server here! https://discord.gg/UnHyHkhbga`
+    'kabedon':'{user} kabedons {message} :OO',
+    'about':`Hey guys! I'm Ryan currently living in Asia, I do co-working streams and I am a second-year computer science college student :D`,
+    'intro':`Hey guys! I'm Ryan currently living in Asia, I do co-working streams and I am a second-year computer science college student :D`,
+    'discord':`{user} Come join the Discord server here! https://discord.gg/UnHyHkhbga`,
+    'commands':`{user} Check out my commands here! https://github.com/liyunze-coding/comfy-twitch-bot`
 }
 
 const broadcaster_commands = {
@@ -23,20 +25,24 @@ const broadcaster_commands = {
     'raid2':'RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid RYANRAID TombRaid'
 }
 
-var streamers = {
-	'pinsaregood':true, 
-	'4l1c3_0':true, 
-	'auspexonegaming':true, 
-	'unknownnie':true,
-	'charliosaurus':true, 
-	'cloudyjoel':true, 
-	'theyolotato':true,
-	'itsbrandonut':true,
-	'berryspace':true,
-	'studypaws':true,
-	'wrongarrow':true,
-	'arcaneXVIII':true,
-	'bubxmicn':true
+// streamers I highly recommend
+// incomplete, if you wanna be in the list, just leave a message in my chat :D
+let streamers_to_shoutout = [
+    'pinsaregood',     '4l1c3_0',
+    'auspexonegaming', 'unknownnie',
+    'charliosaurus',   'cloudyjoel',
+    'theyolotato',     'itsbrandonut',
+    'berryspace',      'studypaws',
+    'wrongarrow',      'arcaneXVIII',
+    'bubxmicn',        'tophurino',
+    'wasmishtaken_',   'areeke',
+    'KhrowV',          'brisim_claimhte'
+]
+
+var streamers = {};
+
+for (s of streamers_to_shoutout){
+    streamers[s.toLowerCase()] = true;
 }
 
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
@@ -45,13 +51,15 @@ ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
         reply = reply.replace('{user}', `@${user}`);
         reply = reply.replace('{message}', `${message}`)
         ComfyJS.Say(reply);
-    } else if (broadcaster_commands[command]){
-        if (flags.broadcaster){
-            let reply = broadcaster_commands[command];
-            reply = reply.replace('{user}', `@${user}`);
-            reply = reply.replace('{message}', `${message}`)
-            ComfyJS.Say(reply);
-        }
+    } else if (broadcaster_commands[command] && flags.broadcaster){
+        let reply = broadcaster_commands[command];
+        reply = reply.replace('{user}', `@${user}`);
+        reply = reply.replace('{message}', `${message}`)
+        ComfyJS.Say(reply);
+    } else if (command === 'time'){
+        let d = new Date();
+        let localtime = d.toLocaleTimeString('en-US', { hour12: true });
+        ComfyJS.Say(`${user} it is currently ${localtime}`);
     }
 }
 
